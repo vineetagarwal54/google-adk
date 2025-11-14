0f36b5d5d09db3165f3545bc47228f0213e7efda
vineetagarwal54 <vineetagarwal540@gmail.com>
2025-11-13 19:55:45 -0500

    SequentialAgent

---
+++ SequentialAgents.py
@@ -0,0 +1,84 @@
+import os
+import asyncio
+from unittest import runner
+from urllib import response
+import warnings
+import logging
+import dotenv
+from google.adk.agents import Agent, ParallelAgent, SequentialAgent, LoopAgent
+from google.adk.models.google_llm import Gemini
+from google.adk.runners import InMemoryRunner
+from google.adk.tools import AgentTool, FunctionTool, google_search
+from google.genai import types
+
+# Suppress warnings for cleaner output
+warnings.filterwarnings('ignore')
+logging.getLogger('google').setLevel(logging.ERROR)
+
+# Load environment variables
+dotenv.load_dotenv()    
+Google_API_key = os.getenv('GOOGLE_API_KEY')
+retry_config = types.HttpRetryOptions(
+    attempts = 5,
+    exp_base = 7,
+    initial_delay = 1,
+    http_status_codes=[429, 500, 503, 504] # Retry on these HTTP errors
+)
+
+#outline agent definition
+# Outline Agent: Creates the initial blog post outline.
+outline_agent = Agent(
+    name="OutlineAgent",
+    model=Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_options=retry_config
+    ),
+    instruction="""Create a blog outline for the given topic with:
+    1. A catchy headline
+    2. An introduction hook
+    3. 3-5 main sections with 2-3 bullet points for each
+    4. A concluding thought""",
+    output_key="blog_outline",  # The result of this agent will be stored in the session state with this key.
+)
+
+#Writing agent definition
+# Writer Agent: Writes the full blog post based on the outline from the previous agent.
+writer_agent = Agent(
+    name="WriterAgent",
+    model=Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_options=retry_config
+    ),
+    # The `{blog_outline}` placeholder automatically injects the state value from the previous agent's output.
+    instruction="""Following this outline strictly: {blog_outline}
+    Write a brief, 200 to 300-word blog post with an engaging and informative tone.""",
+    output_key="blog_draft",  # The result of this agent will be stored with this key.
+)
+
+#editing agent definition
+editor_agent = Agent(
+    name="EditorAgent",
+    model=Gemini(
+        model="gemini-2.5-flash-lite",
+        retry_options=retry_config
+    ),
+    # This agent receives the `{blog_draft}` from the writer agent's output.
+    instruction="""Edit this draft: {blog_draft}
+    Your task is to polish the text by fixing any grammatical errors, improving the flow and sentence structure, and enhancing overall clarity.""",
+    output_key="final_blog",  # This is the final output of the entire pipeline.
+)
+
+root_agent = SequentialAgent(
+    name="BlogPipelineAgent",
+    sub_agents=[outline_agent, writer_agent, editor_agent],
+)
+
+
+async def main():
+    runner = InMemoryRunner(agent=root_agent)
+    response = await runner.run_debug("The Future of Artificial Intelligence in Everyday Life")
+    print("Final Blog Post:\n")
+    print(response)
+
+if __name__ == "__main__":
+    asyncio.run(main())
